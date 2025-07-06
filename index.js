03e091d5620836c7a75f01b1778611ec43f25f24
Neschetnyi <a.neschetnyi@gmail.com>
2025-07-06 22:52:34 +0500

    2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 const MONGO_URI = process.env.MONGO_URI;
 let db, collection;
