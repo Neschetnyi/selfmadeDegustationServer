9ef517c24708d2e073c97e0a7a0290e65950c846
Neschetnyi <a.neschetnyi@gmail.com>
2025-07-09 15:57:56 +0500

    13

--- controllers/getDailyController.js
+++ controllers/getDailyController.js
@@ -83,7 +83,9 @@ export async function getDailyData(
     probsDataRowCellArray.forEach((element) => {
       porbsToDegustate.push({
         lable: probsData.values[element[0]][2],
-        aging: probsData.values[element[0]][element[1]],
+        aging: probsData.values[element[0]][element[1]]
+          .toISOString()
+          .split("T")[0],
       });
     });
 
