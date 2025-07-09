5c0dff393fb7070cc465063259da85046055b203
Neschetnyi <a.neschetnyi@gmail.com>
2025-07-09 16:57:50 +0500

    добавил фильтрацию и разрешил корсы

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 
 export default app;
